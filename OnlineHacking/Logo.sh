43fdf80ed428b72912d4295961c72730ab76fe73
Vicky Kumar <imvickykumar999@gmail.com>
2025-03-14 14:23:50 +0530

    created decodeit.py

--- OnlineHacking/Logo.sh
+++ OnlineHacking/Logo.sh
@@ -20,11 +20,11 @@ echo -e "\e[95m
 ██   ██ ██   ██ ██   ██ ██  ██      ██ ███ ██ ██      ██   ██ 
 ██████  ██   ██ ██   ██ ██   ██      ███ ███  ███████ ██████  \e[0m\n"
 echo -e "\e[95m \e[0m\n"
-  echo""    
-  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;95m\e[0m\e[1;32m Host Daek Website \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
-  echo ""
-   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m\e[1;32m\e[0m\e[1;33m]\e[0m'
-   echo ""
+echo""    
+echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;95m\e[0m\e[1;32m Host Daek Website \e[0m''\e[1;37m\e[0m\e[1;37m [v 1.1] \e[0m'                                       
+echo ""
+echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Github : \e[36mhttps://github.com/OnlineHacKing/HostDarkWeb \e[0m\e[1;32m\e[0m\e[1;33m]\e[0m'
+echo ""
 sleep 1
 echo ""
 echo -e "              \033[1;91m  Author   \033[1;90m:  \033[1;95mOnline HacKing \033[1;97m(SUMAN)"
@@ -34,4 +34,3 @@ echo -e "              \033[1;97m  Website   \033[1;90m: \033[1;93mwww.onlinehac
 echo ""
 echo -e ""
 sleep 1                                               
-
