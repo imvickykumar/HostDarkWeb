e4ced8e18ffb7b79909b2eab50d90ab73e70fada
Vicky Kumar <imvickykumar999@gmail.com>
2025-03-14 20:03:16 +0530

    restored

--- OnlineHacking/Logo2.sh
+++ OnlineHacking/Logo2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo ""
 echo -e "\e[92m
-\e[34m
+SUMAN ©\e[34m
          ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
         ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
         ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
@@ -18,4 +18,6 @@ echo -e "\e[92m
 ONLINE HACKING			  					  							  
           \e[m"
 echo -e "     \e[1m    \e[95mwww.onlinehacking.in \e[92m/ \e[0m\e[93mwww.onlinehacking.xyz \e[0m\n  \e[21m "
-echo ""
+  
+
+		     echo ""
